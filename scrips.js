32ecae70adaa7a10646682dbb3eba01c8ce0b2ee
Ha Quang Tien <tienhq@devcamp.edu.vn>
2023-01-30 23:21:52 +0700

    commit

---
+++ scrips.js
@@ -0,0 +1,45 @@
+
+const items = [
+    {
+        id:1,
+        name: "Leather blog",
+        description: "Distressed Tote Leather Blog",
+        price: "216,27 $",
+        imageUrl: "image/"
+    },
+    {
+        id:2,
+        name: "Elastic Straps",
+        description: "Womens Cognac Leather",
+        price: "195,13 $",
+        imageUrl: anh49
+    },
+    {
+        id:3,
+        name: "Leather blog",
+        description: "Distressed Tote Leather Blog",
+        price: "910,76 $",
+        imageUrl: anh51
+    },
+    {
+        id:4,
+        name: "",
+        description: "High Top Canvas Shoes",
+        price: "341,02 $",
+        imageUrl: anh53
+    },
+    {
+        id:5,
+        name: "Wedding Ring Set",
+        description: "Rose Gold Moissanite Vintage",
+        price: "3,341,13 $",
+        imageUrl: anh55
+    },
+    {
+        id:6,
+        name: "Plated Handmade",
+        description: "Blue Topaz Bangle 24K Gold",
+        price: "789,09 $",
+        imageUrl: anh57
+    }
+]
